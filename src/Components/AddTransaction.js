9e2d6a1829eb1ede9150cc7ee091ac8eafaa6bf3
faiz260 <59044640+faiz260@users.noreply.github.com>
2020-06-17 16:53:29 +0100

    first commit

---
+++ src/Components/AddTransaction.js
@@ -0,0 +1,48 @@
+import React, {useState, useContext} from 'react';
+import { GlobalContext } from '../Context/GlobalState';
+
+export const AddTransaction = () => {
+
+    const {addTransaction} = useContext(GlobalContext);
+
+    const [text, setText] = useState("");
+    const [amount, setAmount] = useState(0);
+
+    const submit = (e) => {
+       e.preventDefault();
+       setAmount({
+           amount: 0
+       })
+
+        const newTransaction = {
+           id: Math.floor(Math.random() * 100000),
+           text,
+           amount: +amount
+       }
+
+       addTransaction(newTransaction);
+    }
+
+    return (
+        <div  className="addTransaction"  >
+            <h3>Add Transaction</h3> 
+            <hr/>
+            <h4>Text</h4>
+            <form onSubmit={submit} >
+                <input
+                 type="text" 
+                 value={text}  
+                 onChange={(e)=>setText(e.target.value)}
+                  placeholder="Enter Text here" ></input>
+                <h4>Amount</h4>
+                <p>(+ve for Income, -ve for expense)</p>
+                <input type="number" value={amount} onChange={(e)=>setAmount(e.target.value)}  
+                 placeholder="Enter Amount here" ></input>
+                <br/>
+                <br/>
+               {/* <Button variant="contained" >Add Transaction</Button  > */}
+               <button>Add Transaction</button>
+            </form>
+        </div>
+    )
+}
---
+++ src/Components/header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div className="header "
+        //  style={{
+        //     display: "flex", alignItems: "center", justifyContent: "center",
+        //     // width: "400px",
+        //     // height: "100px",
+        //     border: "1px solid black",
+        //     boxShadow: "5px 5px 15px 5px rgb(163, 163, 163)",
+        // }
+        // }  
+        >
+            <h1>Expense Tracker</h1>
+        </div>
+    )
+}
